d82092215d293d29a7b7a8f1d8705c637433f84f
hazelgc2 <hazelo235@gmail.com>
2024-10-20 20:17:37 -0600

    Initial Commit

---
+++ constrains/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  constrains
+//
+//  Created by Hazel Alain on 20/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ constrainsTests/constrainsTests.swift
@@ -0,0 +1,17 @@
+//
+//  constrainsTests.swift
+//  constrainsTests
+//
+//  Created by Hazel Alain on 20/10/24.
+//
+
+import Testing
+@testable import constrains
+
+struct constrainsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
